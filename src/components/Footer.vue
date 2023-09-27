87f7a13b83a764ffcd07bf95ab1a37c331253c72
Tolga Egilmezel <tolga@egilmezel.com>
2023-09-27 19:04:35 +0300

    Vue init project Vue Components Component Slots

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MenuItem from "@/components/Menu/MenuItem.vue";
+</script>
+
+<template>
+    <footer>
+        2023 &copy; Robot Dreams
+        <MenuItem title="My Web Site"></MenuItem>
+        <a href="#">Footer Link</a>
+    </footer>
+</template>
---
+++ src/components/Menu/Menu.vue
@@ -0,0 +1,20 @@
+<script setup>
+import MenuItem from "@/components/Menu/MenuItem.vue"
+
+const menuFirstTitle = "Ana Sayfa 2";
+const menuSecondTitle = "Iletisim 1"
+</script>
+
+<template>
+    <nav>
+        <MenuItem :title="menuFirstTitle" emoji="🦄"></MenuItem>
+        <MenuItem :title="menuSecondTitle"></MenuItem>
+        <MenuItem title="Yardim Sayfasi">
+            <template #optional>
+                🦖 <strong>Robot Dreams</strong>
+                <p>Lorem ipsum dolor sit amet.</p>
+                <button>Button</button>
+            </template>
+        </MenuItem>
+    </nav>
+</template>
